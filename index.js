4c36c060309e67748b954c20f7f349855d41b952
saurav sharma <sauravsharma@sauravs-MacBook-Air.local>
2023-10-31 14:21:34 +0530

    dns

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dns = require('dns2');
+const app = express();
+
+const dnsServer = dns.createServer();
+dnsServer.listen(53);
+dnsServer.on('request', (request, response) => {
+    const { question } = request;
+    console.log(question);
+    // Your logic to handle DNS requests and provide responses
+    response.end();
+});
+app.get('/', (req, res) => {
+    res.send('MERN DNS Service is running!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
